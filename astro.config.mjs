a43ed8cc6580805a88eeca657cfa09bd6d7a6c3f
Roberto Navarro Cuesta <robertonavarrocuesta10@gmail.com>
2026-06-08 16:52:36 +0200

    Primer commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import vercel from "@astrojs/vercel";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
+});
